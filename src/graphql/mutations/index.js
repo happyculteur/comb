bf158481e2978a8a78ad9d39c4fbc2e5d32b853b
davidbillamboz <davidbillamboz@gmail.com>
2017-12-18 23:26:06 +0100

    Prepare files for futures mutations and queries

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,5 +1,9 @@
 import createUserMutation from './createUserMutation';
+// import updateUserMutation from './updateUserMutation';
+// import deleteUserByIdMutation from './deleteUserByIdMutation';
 
 export default {
   createUser: createUserMutation,
+  // updateUser: updateUserMutation,
+  // deleteUser: deleteUserByIdMutation,
 };
--- src/graphql/queries/index.js
+++ src/graphql/queries/index.js
@@ -1,5 +1,9 @@
 import getUserByIdQuery from './getUserByIdQuery';
+// import searchUsersByLocationQuery from './searchUsersByLocationQuery';
+// import getUsersQuery from './getUsersQuery';
 
 export default {
   getUserById: getUserByIdQuery,
+  // getUsers: getUsersQuery,
+  // searchUsersByLocation: searchUsersByLocationQuery,
 };
