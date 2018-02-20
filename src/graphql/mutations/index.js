ff1a2cb020d3fb1a694aee9df690773380a0c11d
Klemele <lemele.kevin@gmail.com>
2018-02-20 21:51:10 +0100

    Cleaning

--- src/graphql/mutations/index.js
+++
@@ -1,9 +0,0 @@
-import createUserMutation from './createUserMutation';
-// import updateUserMutation from './updateUserMutation';
-// import deleteUserByIdMutation from './deleteUserByIdMutation';
-
-export default {
-  createUser: createUserMutation,
-  // updateUser: updateUserMutation,
-  // deleteUser: deleteUserByIdMutation,
-};
--- src/graphql/queries/index.js
+++
@@ -1,9 +0,0 @@
-import getUserByIdQuery from './getUserByIdQuery';
-// import searchUsersByLocationQuery from './searchUsersByLocationQuery';
-// import getUsersQuery from './getUsersQuery';
-
-export default {
-  getUserById: getUserByIdQuery,
-  // getUsers: getUsersQuery,
-  // searchUsersByLocation: searchUsersByLocationQuery,
-};
